c06be4f01017a830b16482e9357c3f4e8a195f3c
hoshinoyebi <a0970616978@gmail.com>
2026-09-12 15:10:24 +0000

    Implement square function

--- week01/functions_basic.cpp
+++ week01/functions_basic.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
+int square(int x){
+    return x*x;
+}
 int main(){
-
-
-
-
-
-
-
-
-
+    int a;
+    cin >> a;
+    cout << square(a);
     return 0;
 }
+
